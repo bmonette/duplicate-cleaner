041fd197c9a6c244cde8c0de1bb3e16ad40d421b
bmonette <benoitmonette80@gmail.com>
2026-03-16 15:53:38 -0400

    Added progress update support

--- deduplicator.py
+++ deduplicator.py
@@ -2,18 +2,22 @@ from collections import defaultdict
 from hasher import calculate_hash
 
 
-def group_files_by_hash(files):
+def group_files_by_hash(files, progress_callback=None):
     hash_map = defaultdict(list)
+    total_files = len(files)
 
-    for file in files:
+    for index, file in enumerate(files, start=1):
         file_hash = calculate_hash(file)
         hash_map[file_hash].append(file)
+
+        if progress_callback is not None:
+            progress_callback(index, total_files, file)
 
     return hash_map
 
 
-def find_duplicates(files):
-    grouped = group_files_by_hash(files)
+def find_duplicates(files, progress_callback=None):
+    grouped = group_files_by_hash(files, progress_callback=progress_callback)
 
     duplicates = []
 
